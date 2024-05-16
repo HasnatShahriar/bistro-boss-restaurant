e1306259d8f74977b971a83eb1b07af675a5ca34
HasnatShahriar <shahriarhriday94@gmail.com>
2024-05-16 15:44:02 +0600

    added banner & slider in home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,12 @@
+import Banner from "./Banner/Banner";
+import Category from "./Category/Category";
 
 
 const Home = () => {
   return (
     <div>
-      <h2>this is home</h2>
+      <Banner/>
+      <Category/>
     </div>
   );
 };
