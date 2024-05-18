65ad29a8a564f267c8e0d51307b6da60fef82d72
HasnatShahriar <shahriarhriday94@gmail.com>
2024-05-18 14:17:30 +0600

    added popular food menu in home page

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,13 +1,30 @@
+import { useEffect, useState } from "react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import MenuItem from "../../../Shared/MenuItem/MenuItem";
 
 
 const PopularMenu = () => {
+  const [menu,setMenu] = useState([])
+  useEffect(()=>{
+    fetch('menu.json')
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      const popularItems = data.filter(item => item.category === 'popular')
+      setMenu(popularItems)
+    })
+  },[])
   return (
-    <section>
+    <section className="mb-12">
       <SectionTitle
       heading={'From Our Menu'}
       subHeading={'Popular Items'}
       />
+      <div className="grid md:grid-cols-2 gap-6">
+        {
+          menu.map(item => <MenuItem key={item._id} item={item}/>)
+        }
+      </div>
     </section>
   );
 };
