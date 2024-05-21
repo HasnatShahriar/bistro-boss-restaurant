cc766d86ebd47f46d6598f53f634e4009c0f8e89
HasnatShahriar <shahriarhriday94@gmail.com>
2024-05-21 13:50:04 +0700

    added private route

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -5,6 +5,8 @@ import Menu from "../pages/Menu/Menu";
 import Order from "../pages/Order/Order";
 import Login from "../pages/Login/Login";
 import SignUp from "../pages/SignUp/SignUp";
+import PrivateRoute from "./PrivateRoute";
+import Secret from "../Shared/Secret/Secret";
 
 const router = createBrowserRouter([
   {
@@ -30,6 +32,10 @@ const router = createBrowserRouter([
       {
         path: '/signUp',
         element: <SignUp/>
+      },
+      {
+        path:"secret",
+        element: <PrivateRoute><Secret/></PrivateRoute>
       }
     ]
   },
--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -13,6 +13,7 @@ const Navbar = () => {
     <li><Link to='/'>Home</Link></li>
     <li><Link to='/menu'>Our Menu</Link></li>
     <li><Link to='/order/salad'>Order</Link></li>
+    <li><Link to='/secret'>Secret</Link></li>
 
     {
       user ? <><button onClick={handleLogOut} className="btn btn-ghost btn-sm">Log Out</button></> : <><li><Link to='/login'>Login</Link></li></>
